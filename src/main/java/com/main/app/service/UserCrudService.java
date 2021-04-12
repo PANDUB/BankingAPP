4b50b68e3af4b59a377d1d5f288efb13ece442ab
Pandu Rangadu <akkisahi@MacBook-Pro.broadband>
2021-04-12 21:37:28 +0100

    added changes for jenkin piple line

--- src/main/java/com/main/app/service/UserCrudService.java
+++ src/main/java/com/main/app/service/UserCrudService.java
@@ -2,7 +2,19 @@ package com.main.app.service;
 
 import com.main.app.db.entity.User;
 
+import java.util.concurrent.CompletableFuture;
+
 public interface UserCrudService {
 
     public User createUser(User user);
+
+    public static void main(String[] args) {
+
+
+        CompletableFuture<String> co = new CompletableFuture<>();
+        co.thenApply(r -> "heelo");
+
+       // CompletableFuture<String> cosync = CompletableFuture.runAsync();
+
+    }
 }
